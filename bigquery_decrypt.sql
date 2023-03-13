d619d886fa4adafaa82b98add88fc4e422b4347f
Richard He <shenghua.he@rocketech.co.uk>
2023-03-13 23:51:44 +0000

    WIP

--- bigquery_decrypt.sql
+++ bigquery_decrypt.sql
@@ -1,3 +1,4 @@
+--- Query returns the following error: Query error: AEAD.DECRYPT_STRING failed: Keyset deserialization failed: Error reading keyset data: Could not parse the input stream as a Keyset-proto. at [5:1]
 DECLARE kms_resource_name STRING;
 SET kms_resource_name = 'gcp-kms://projects/rocketech-de-pgcp-sandbox/locations/europe-west2/keyRings/aead-example/cryptoKeys/symmetric_demo';
 
